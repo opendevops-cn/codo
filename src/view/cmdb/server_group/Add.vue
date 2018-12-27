0d315cb2c32ed423acfc23b49ed692fbf8440343
yangmv <yangmv@liubiqiandeMacBook-Pro.local>
2018-12-27 18:04:25 +0800

    修复HostGroup

--- src/view/cmdb/server_group/Add.vue
+++ src/view/cmdb/server_group/Add.vue
@@ -1,7 +1,7 @@
 <template>
 <!-- <div> -->
   <!-- <Card> -->
-<Modal width="600px;" v-model="dialog.show"  :title="dialog.title" :loading=true :footer-hide=true> 
+<Modal width="600px;" v-model="dialog.show"  :title="dialog.title" :loading=true :footer-hide=true>
     <Form ref="formData" :model="formData" :rules="ruleValidate" :label-width="80">
         <FormItem label="组名" prop="name">
             <Input v-model="formData.name" placeholder=""></Input>
@@ -32,86 +32,86 @@
 <!-- </div> -->
 </template>
 <script>
-    import { addServerGroup } from '@/api/cmdb/server_group'
-    import { getTableData } from '@/api/cmdb/server'
-    //import { getAppList } from '@/api/app'
-    export default {
-        name: 'add',
-        data () {
-            return {
-                ServerList: [],
-                serverTitle: ['可关联','已关联'],
-                ruleValidate: {
-                    name: [
-                        { required: true, message: '项目名称不能为空', trigger: 'blur' }
-                    ]
-                }
-            }
-        },
-        methods: {
-            serverHandleChange (newTargetKeys) {
-                this.formData.server_set = newTargetKeys;
-            },
-            serverFilter (data, query) {
-                return data.label.indexOf(query) > -1;
-            },
-            handleSubmit (name) {
-                this.$refs[name].validate((valid) => {
-                    if (valid) {
-                        // add => post => /api/cmdb/server_group/
-                        // edit => put => /api/cmdb/server_group/1
-                        const url = this.dialog.option == 'add' ? '/' : `/${this.formData.id}/`;
-                        const action = this.dialog.option == 'add' ? 'post' : 'put';
-                        // console.log(this.formData)
-                        addServerGroup(this.formData,url,action).then(res => {
-                            console.log(res)
-                            if (res.status){
-                                this.$Message.success({
-                                    content: '主机组添加成功',
-                                    duration: 3
-                                });
-                                this.$emit('e-close');
-                                this.$emit('e-update');
-                            }else{
-                                this.$Message.error({
-                                    content: res.data.msg,
-                                    duration: 3
-                                }); 
-                            }
-                        }).catch(error =>{
-                            this.$Message.error({
-                                content: JSON.stringify(error.response.data),
-                                duration: 10
-                            });
-                        });
-                    } else {
-                        this.$Message.error({
-                                content: '请检查必选项',
-                                duration: 3
-                        });
-                    }
-                })
-            },
-            handleReset (name) {
-                this.$refs[name].resetFields();
-            }
-
-        },
-        props:{
-            dialog: Object,
-            formData: Object
-        },
-        mounted(){
-            const params = {pageNum: 1,pageSize: 10000}
-            getTableData(params).then(res => {
-                const data = res.data.data
-                for(var item in data){
-                    this.ServerList.push({
-                        key: data[item].id,
-                        label: data[item].hostname
-                    })
-                }
-            }) 
-        }
+import { addServerGroup } from '@/api/cmdb/server_group'
+import { getTableData } from '@/api/cmdb/server'
+// import { getAppList } from '@/api/app'
+export default {
+  name: 'add',
+  data () {
+    return {
+      ServerList: [],
+      serverTitle: ['可关联', '已关联'],
+      ruleValidate: {
+        name: [
+          { required: true, message: '项目名称不能为空', trigger: 'blur' }
+        ]
+      }
     }
+  },
+  methods: {
+    serverHandleChange (newTargetKeys) {
+      this.formData.server_set = newTargetKeys
+    },
+    serverFilter (data, query) {
+      return data.label.indexOf(query) > -1
+    },
+    handleSubmit (name) {
+      this.$refs[name].validate((valid) => {
+        if (valid) {
+          // add => post => /api/cmdb/server_group/
+          // edit => put => /api/cmdb/server_group/1
+          const url = this.dialog.option == 'add' ? '/' : `/${this.formData.id}/`
+          const action = this.dialog.option == 'add' ? 'post' : 'put'
+          // console.log(this.formData)
+          addServerGroup(this.formData, url, action).then(res => {
+            console.log(res)
+            if (res.status) {
+              this.$Message.success({
+                content: '主机组添加成功',
+                duration: 3
+              })
+              this.$emit('e-close')
+              this.$emit('e-update')
+            } else {
+              this.$Message.error({
+                content: res.data.msg,
+                duration: 3
+              })
+            }
+          }).catch(error => {
+            this.$Message.error({
+              content: JSON.stringify(error.response.data),
+              duration: 10
+            })
+          })
+        } else {
+          this.$Message.error({
+            content: '请检查必选项',
+            duration: 3
+          })
+        }
+      })
+    },
+    handleReset (name) {
+      this.$refs[name].resetFields()
+    }
+
+  },
+  props: {
+    dialog: Object,
+    formData: Object
+  },
+  mounted () {
+    const params = {pageNum: 1, pageSize: 10000}
+    getTableData(params).then(res => {
+      const data = res.data.data
+      for (var item in data) {
+        this.ServerList.push({
+          key: data[item].id,
+          label: data[item].hostname
+        })
+      }
+    })
+  }
+}
 </script>
