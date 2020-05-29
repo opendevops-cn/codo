20507e19dec15141043367f8889075f75ee1d3d8
tuanzi <shenshuo@papegames.net>
2020-05-29 14:05:20 +0800

    修改vue.config

--- vue.config.js
+++ vue.config.js
@@ -37,6 +37,6 @@ module.exports = {
     // 这里写你调用接口的基础路径，来解决跨域，如果设置了代理，那你本地开发环境的axios的baseUrl要写为 '' ，即空字符串
     devServer: {
         // proxy: 'http://172.16.0.223:9800/'
-        proxy: 'http://demo.opendevops.cn/api/'
+        proxy: 'http://gw.opendevops.cn/api/'
     }
 }
